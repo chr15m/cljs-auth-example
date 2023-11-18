d8a63ccdce2294cfa2689853b986b7b9aefe1852
Chris McCormick <chris@mccormick.cx>
2023-11-18 12:51:15 +0800

    Implemented auth demo and doc.

---
+++ README.md
@@ -0,0 +1,7 @@
+Minimal ClojureScript API auth example.
+
+`make watch` to run.
+
+* Change `auth-test-endpoint` to `auth-failure.json` on line 7 to demonstrate failing auth that then shows the login view. You could also redirect to your app's login page (and have it redirect back to the frontend after logging in) in the `init` function.
+* Note on line 57 there is an artificial delay to demonstrate how the loading spinner waits until the auth data has been fetched.
+* In a real app you would create a `/api/auth` endpoint which returns the JSON (or transit) formatted auth data for the current user session instead of requesting json files. The json files are just for demonstration purposes.
--- src/cljs_auth_example/core.cljs
+++ src/cljs_auth_example/core.cljs
@@ -1,19 +1,59 @@
 (ns cljs-auth-example.core
   (:require
     [reagent.core :as r]
-    [reagent.dom :as rdom]))
+    [reagent.dom :as rdom]
+    [promesa.core :as p]))
+
+(def auth-test-endpoint
+  ; "auth-failure.json"
+  "auth-success.json")
 
 (defonce state (r/atom {}))
 
-(defn component-main [_state]
+(defn view:app [state]
+  (let [username (get-in @state [:auth :username])]
+    [:div
+     [:h1 "cljs-auth-example"]
+     [:p "Welcome to the app, " username "!"]
+     [:button {:on-click #(swap! state update-in [:clicks] inc)}
+      "click me"] " (clicked " (:clicks @state) " times)"]))
+
+(defn view:login-screen [state]
   [:div
-   [:h1 "cljs-auth-example"]
-   [:p "Welcome to the app!"]
-   [:button {:on-click #(js/alert "Hello world!")} "click me"]])
+   [:h1 "please log in"]
+   [:form {:on-submit
+           (fn [ev]
+             (.preventDefault ev)
+             (js/alert "now post to auth endpoint
+                       and update :auth with the result"))}
+    [:input {:placeholder "username"
+             :on-change #(swap! state assoc :username
+                                (-> % .-target .-value))
+             :value (:username @state)}]
+    [:input {:placeholder "password"
+             :type "password"
+             :on-change #(swap! state assoc :password
+                                (-> % .-target .-value))
+             :value (:password @state)}]
+    [:button "submit"]]])
+
+(defn view:main [state]
+  (let [authenticated? (get-in @state [:auth :authenticated])]
+    (js/console.log @state)
+    (if authenticated?
+      [view:app state]
+      [view:login-screen state])))
 
 (defn start {:dev/after-load true} []
-  (rdom/render [component-main state]
+  (rdom/render [view:main state]
                (js/document.getElementById "app")))
 
 (defn init []
-  (start))
+  (p/let [response (js/fetch (str "/" auth-test-endpoint))
+          json (.json response)
+          auth (js->clj json :keywordize-keys true)]
+    ; artificial delay to demonstrate
+    ; the loading spinner while auth loads
+    (p/delay 1000)
+    (swap! state assoc :auth auth) 
+    (start)))
